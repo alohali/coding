7f92120b6663ccf7154644254de729d43e0e0f91
alohal <dtcrong@github.com>
2016-11-23 21:26:57 +0800

    adding split models

--- practice/310.cpp
+++ practice/310.cpp
@@ -1,125 +1,69 @@
-128. Longest Consecutive Sequence
-class Solution {
-    vector<int> parent;
-    vector<int> rank;
-public:
-    int numIslands(vector<vector<char>>& grid) {
-        int nR = grid.size();
-        if(nR == 0) return 0;
-        int nC = grid[0].size();
-        int num=0;
-        parent.resize(nR*nC, 0);
-        rank.resize(nR*nC, 0);
-        for(int i=0; i<parent.size(); i++) {
-            parent[i] = i;
-        }
-        for(int i=0; i<nR; i++) {
-            for(int j=0; j<nC; j++) {
-                if(grid[i][j] == '0') continue;
-                calculateNumIslands(i, j, grid, num);
-            }
-        }
-        return(num);
-    }
-    
-    void calculateNumIslands(int r, int c, vector<vector<char>>& grid, int &num) {
-        int cell = 0, cellUp = 0, cellLeft = 0;
-        int nR = grid.size();
-        int nC = grid[0].size();
-        cell = r * nC + c;
-        cellLeft = r * nC + c-1;
-        cellUp = (r-1) * nC + c;
-        num += 1;
-        if(c > 0 && grid[r][c-1] == '1') {
-            merge(cell, cellLeft);
-            num -= 1;
-        }
-        int rootOfCell = getRoot(cell);
-        int rootOfCellUp = 0;
-        if(r > 0 && grid[r-1][c] == '1') {
-            rootOfCellUp = getRoot(cellUp);
-            if(rootOfCell != rootOfCellUp) {
-                merge(cell, cellUp);
-                num -= 1;
-            }
-        }
-    }
-    
-    void merge(int a, int b) {
-        int root_a = getRoot(a);
-        int root_b = getRoot(b);
-        int rank_a = rank[root_a];
-        int rank_b = rank[root_b];
-        if(rank_a > rank_b) {
-            parent[root_b] = root_a;
-        } else {
-            parent[root_a] = root_b;
-            if(rank_a == rank_b) {
-                pathCompress(b);    
-            }
-        }
-    }
-    
-    int getRoot(int x) {
-        if(parent[x] == x) {
-            return x;
-        }
-        return(getRoot(parent[x]));
-    }
-    
-    int pathCompress(int x) {
-        if(parent[x] == x) {
-            return x;
-        }
-        parent[x] = pathCompress(parent[x]);
-        return(parent[x]);
-    }
-};
-
-
-
-
-
-
-vector<int> findMinHeightTrees(int n, vector<pair<int, int>>& edges) {
-        vector<vector<int>> neighbors(n);
-        int numNeighbors[n]{};
-        
-        for (auto e : edges) {                                  // update stats based on edges
-            neighbors[e.first].push_back(e.second), numNeighbors[e.first]++;
-            neighbors[e.second].push_back(e.first), numNeighbors[e.second]++;
-        }
-        
-        queue<int> leaves;                                      // use queue to record leaves/sinks
-        for (int i = 0; i < n; i++)
-            if (numNeighbors[i] <= 1) { leaves.push(i); }       // '<' is for case of n == 1
-            
-        while (n > 2) {                                         // BFS
-            n -= leaves.size();                                 // decrease n for the nodes in leaves
-            for (int len = leaves.size(); len; len--) {         // process nodes in leaves
-                int cur = leaves.front(); leaves.pop();
-                
-                for (auto nb : neighbors[cur])                  // update leaves
-                    if (--numNeighbors[nb] == 1) { leaves.push(nb); }
-            }
-        }
-        
-        vector<int> ans;
-        while (leaves.size()) { 
-            ans.push_back(leaves.front());                      // construct answer vector
-            leaves.pop();
-        }
-        return ans;
-}
-
-
-
-
-
 ////310. Minimum Height Trees
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<pair<int, int>>& edges) {
-        
+        vector<int>ret;
+        if(n>2) {
+            vector<unordered_set<int>> graph = makeGraph(n, edges);
+            int Height;
+            int root = bfs(0,graph, Height);
+            bfs(root, graph, Height);
+            ret=getRoots(root, graph, Height);
+        }else if(n==1){
+            ret.push_back(0);
+        }else if(n==2){
+            ret.push_back(0);
+            ret.push_back(1);
+        }
+        return ret;
+    }
+
+    vector<unordered_set<int>> makeGraph(int n, const vector<pair<int, int>>& edges){
+        vector<unordered_set<int>> graph(n);
+        for(auto edge : edges){
+            graph[edge.first].insert(edge.second);
+            graph[edge.second].insert(edge.first);
+        }
+        return graph;
+    }
+
+    int bfs(int root, vector<unordered_set<int>> &graph, int&Height){
+        int ret;
+        queue<int> q0; q0.push(root);
+        vector<int> dist(graph.size(), -1);
+        dist[root] = 0;
+
+        while(!q0.empty()){
+            ret = q0.front(); q0.pop();
+            for(auto neigh : graph[ret]){
+                if(dist[neigh]<0){
+                    q0.push(neigh);
+                    dist[neigh] = dist[ret]+1;
+                }
+            }
+        }
+        Height = dist[ret];
+        return ret;
+    }
+    vector<int> getRoots(int root, vector<unordered_set<int>> &graph, int Height){
+        vector<int> ret;
+        queue<int> q0; q0.push(root);
+        vector<int> dist(graph.size(), -1);
+        dist[root] = 0;
+        int level = Height/2;
+        int level2 = (Height%2)+level;
+        while(!q0.empty()){
+            int node = q0.front(); q0.pop();
+            for(auto neigh : graph[node]){
+                if(dist[neigh]<0){
+                    q0.push(neigh);
+                    dist[neigh] = dist[node]+1;
+                    if((dist[neigh]==level) || dist[neigh]==level2){
+                        ret.push_back(neigh);
+                    }
+                }
+            }
+        }
+        return ret;
     }
 };
---
+++ pyLearn/model.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+import Queue
+from enum import Enum
+
+
+class Type(Enum):
+    FC = 1,
+    CONV = 2,
+    POOL = 3,
+    NORM = 4,
+    DATA = 5,
+    ELEM_WISE = 6,
+    OTHER = 7
+
+
+class Layer:
+    data_size = 1
+
+    def __init__(self):
+        self.split = False
+        self.recombine = False
+        self.w_divide = self.h_divide = 1
+
+    def setParam(self,**kw):
+        self.name = kw['name']
+        self.type = kw['type']
+        self.nchwkpq = kw['nchwkpq']
+        self.pre = kw['pre']
+        self.suc = kw['suc']
+        if self.type in [Type.CONV, Type.POOL]:
+            self.rs, self.uv = kw['rsuv']
+            self.pad = kw['pad']
+        self.weight_size = self.__weight_size()
+        self.output_size = self.__output_size()
+        self.input_size  = self.__input_size()
+        if self.__is_winograd:
+            # add winograd weights into consideration as low L2 hitrate
+            self.memory_footprint = self.input_size + self.output_size + self.weight_size  
+        else:
+            self.memory_footprint = self.input_size + self.output_size 
+
+
+    def __output_size(self):
+        return self.nchwkpq[0] * self.nchwkpq[4] * self.nchwkpq[5] * self.nchwkpq[6] * self.data_size #nkpq
+
+    def __input_size(self):
+        size = 0
+        for pre in self.pre:
+            size += pre.output_size
+        return size
+
+    def __is_winograd(self):
+        if (self.type == Type.CONV and self.rs == 3):  # only 3x3 winograd now
+            if (self.nchwkpq[1] > 3):#c>3
+                return True
+        return False
+
+    def __weight_size(self):
+        w = 0
+        if (self.type == Type.CONV):
+            w = self.rs * self.rs * self.nchwkpq[1] * self.nchwkpq[4] * self.data_size
+            if self.__is_winograd():  # + weight size for winograd
+                w = w * 16 / 9
+        if (self.type == Type.FC):
+            w = reduce(lambda x, y: x * y, self.nchwkpq[1:]) * self.data_size
+        return w
+
+
+
+class SplitNet:
+    def __init__(self, L3size):
+        self.L3size_ = L3size
+
+    def dfs_toposort(self, net):
+        pass
+
+    def split(self, net):
+        for layer in net.Layers:
+            if layer.type == Type.DATA:
+                pass
+            layer.split = __need_split(layer)
+            layer.h_divide, layer.w_divide = __calc_divide(layer)
+            print "split," , layer.name , "w,", layer.h_divide,"h,", layer.w_divide
+            layer.recombine = self.__recombine()
+
+    def __calc_divide(self, layer):
+        if not layer.split:
+            return [1,1]
+        if layer.pre[0].split and not layer.pre[0].recombine: #same as last layer if not recombine
+            return [layer.pre[0].h_divide, layer.pre[0].w_divide]
+
+        io_size = layer.memory_footprint #- layer.weight_size
+        cache   = self.L3size_ #- layer.weight_size, not consider weights now
+        if io_size < cache/2:
+            return [1,2]
+        elif io_size < cache/3:
+            return [1,3]
+        elif io_size < cache/4:
+            return [1,4]
+        elif io_size < cache/6:
+            return [2,3]
+        elif io_size < cache/8:
+            return [2,4]
+        else:
+            print "too large memory!!!!!!! not implemented now~"
+            return [1,1]
+
+
+    def __dfsLifeTime(self):
+        pass
+
+    def __need_split(self, layer):
+        ret = False
+        if layer.pre[0].split and not layer.pre[0].recombine: #same as last layer if not recombine
+            ret = True
+        elif len(layer.suc) > 1:
+            pass
+        elif layer.memory_footprint > self.L3size_:
+            ret = True
+        return ret
+
+    def __recombine(self, layer):
+        print "Haven't implemented"
+        return 1
+
+    def haloSize(self):
+        print "Haven't implemented"
+        return 1
+
+
+class Network:
+    def __init__(self):
+        data0 = Layer()
+        conv0 = Layer()
+        pool0 = Layer()
+        conv1 = Layer()
+        conv2 = Layer()
+        self.root_ = data0
+        self.Layers = [data0, conv0, pool0, conv1, conv2]  # in dfs+toposort
+        data0.setParam(name='data0', type=Type.DATA, nchwkpq= [1, 3, 540, 960, 3, 540, 960],pre=[], suc=[conv0])
+        conv0.setParam(name='conv0', type=Type.CONV, rsuv=[7, 2], pad=3, nchwkpq= [1, 3, 540, 960, 64, 270, 480]  ,pre=[data0], suc=[pool0])
+        pool0.setParam(name='pool0', type=Type.POOL, rsuv=[3, 2], pad=1, nchwkpq= [1, 64, 270, 480, 64, 135, 240] ,pre=[conv0], suc=[conv1])
+        conv1.setParam(name='conv1', type=Type.CONV, rsuv=[1, 0], pad=0, nchwkpq= [1, 64, 135, 240, 64, 135, 240] ,pre=[pool0], suc=[conv2])
+        conv2.setParam(name='conv2', type=Type.CONV, rsuv=[3, 1], pad=1, nchwkpq= [1, 64, 135, 240, 192, 135, 240],pre=[conv1], suc=[])
+
+if __name__ == '__main__':
+    driveNet = Network()
+    splitTest = SplitNet(4096 * 1024)
+    splitTest.split(driveNet)
+
+

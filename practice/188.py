756697d3a1d35cd725e75731386e67db447a6caa
dtcrong <dtcrong@163.com>
2017-01-03 08:32:24 +0800

    ac new 3

---
+++ practice/188.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def maxProfit(self, k, prices):
+        """
+        :type k: int
+        :type prices: List[int]
+        :rtype: int
+        """
+        
+
+if __name__ == "__main__":
+    s0 = Solution()
+    print(s0.maxProfit([1,3,2,4]))
